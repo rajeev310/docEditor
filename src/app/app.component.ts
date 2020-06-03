a791d19b98c5eeaf2a1b95b701118b4a6e09e5b6
rajeev <rajeev.sharma@yash.com>
2020-06-03 15:48:22 +0530

    feat: added text-control editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+declare const readDocument: any;
+declare const saveDocument: any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-editor-app';
+  editorHeight:string = (window.innerHeight - 50)+"px";
+  editorWidth:string = (window.innerWidth - 30)+"px";  
+
+  onChangeLoad(event: any) {
+    readDocument(event);
+  }
+
+  onClickSave() {
+    saveDocument();
+  }  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocumentEditorModule } from '@txtextcontrol/tx-ng-document-editor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DocumentEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
